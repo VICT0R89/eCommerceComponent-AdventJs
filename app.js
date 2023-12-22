17c534bee8569b40a3db4a15039ef02216057175
Victor Gutierrez <victor.gutierrez@wundermanthompson.com>
2023-12-22 09:34:01 -0300

    despues sigo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const $MENU = document.getElementById('menu')
 const $CART = document.getElementById('cart-summary')
 
 const menuItem = props => {
-	const { image, alt, name, price, count } = props
+	const { id, image, alt, name, price, count } = props
   return `
 		<li>
 			<div class="plate">
@@ -11,7 +11,7 @@ const menuItem = props => {
 			<div class="content">
 				<p class="menu-item">${name}</p>
 				<p class="price">${price}</p>
-				<button class=${count > 0 ? 'in-cart' : 'add'}>
+				<button class=${count > 0 ? 'in-cart' : 'add'} id="${id}">
 				${count > 0 ?
 					(`<img src="images/check.svg" alt="Check" />`,
 					'In Cart') :
@@ -70,13 +70,28 @@ const addCartItems = menu => {
 	$CART.innerHTML = `<p class="empty">Your cart is empty.</p>`
 }
 
-addMenuItems(data)
-addCartItems(data)
+const init = () => {
+	addMenuItems(data)
+	addCartItems(data)
+}
+init()
 
 const addToCart = item => {
-
+	data.map(el => {
+		if (el.id == item.id) {
+			el.count = 1
+		}
+	})
+	init()
 }
+
 document.addEventListener("DOMContentLoaded",()=>{
 	const addBtns = document.querySelectorAll('.add')
-	console.log(addBtns)
+	addBtns.forEach(btn => {
+		btn.addEventListener('click', e =>{
+			const id = e.target.id
+			const item = data.filter(el => el.id == id)
+			addToCart(item)
+		})
+	})
 })
